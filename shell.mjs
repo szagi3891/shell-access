e9d3aedecb052f311709098d91601adc8b3ccc8b
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2024-09-01 08:53:26 +0200

    Dada

--- shell.mjs
+++ shell.mjs
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+console.info('Start shell access ...');
+
 var __defProp = Object.defineProperty;
 var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
 var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
